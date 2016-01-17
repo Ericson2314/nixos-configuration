4736d1773acd2e69cbae1ce8ad04824a20166ae1
John Ericson <Ericson2314@Yahoo.com>
2016-01-17 14:22:39 -0800

    Even better synaptic settings

--- synaptics.nix
+++ synaptics.nix
@@ -6,13 +6,20 @@
     enable = true;
 
     minSpeed = "0.1";
-    maxSpeed = "1.5";
+    maxSpeed = "15";
     accelFactor = "0.8";
 
 	tapButtons = false;
+	palmDetect = true;
 
     twoFingerScroll = true;
     vertEdgeScroll = false;
     horizontalScroll = true;
+
+    # "Natural" scrolling
+    additionalOptions = ''
+      Option "VertScrollDelta" "-30"
+      Option "HorizScrollDelta" "-30"
+    '';
   };
 }
