6f3ec18367bea8103b0054ce1cf60279158bade7
John Ericson <Ericson2314@Yahoo.com>
2016-09-15 15:58:20 -0700

    Default console font is clearer on high definition screens

--- common.nix
+++ common.nix
@@ -22,7 +22,7 @@
 
   # Select internationalisation properties.
   i18n = {
-    consoleFont = "lat9w-16";
+    #consoleFont = "lat9w-16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
