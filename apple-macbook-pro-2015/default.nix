647052fb0659028243322502f378ed78f514e3c5
John Ericson <jericson@galois.com>
2016-10-20 14:57:53 -0400

    Use nixos-hardware for apple, now that I factored out

--- apple-macbook-pro-2015/default.nix
+++ apple-macbook-pro-2015/default.nix
@@ -7,16 +7,13 @@
     ../libinput.nix
     ../yubikey.nix
     ../../hardware-configuration.nix # Include the results of the hardware scan.
+    ../../nixos-hardware/apple/macbook-pro-10-1.nix # from the nixos-hardware repo
   ];
 
   # For Broadcom chip
   boot.kernelPackages = pkgs.linuxPackages_4_7;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  services.xserver.videoDrivers = [ "nvidia" "intel" ];
-  hardware.opengl.driSupport32Bit = true;
+  # Supposed to get PTYs back, but didn't
   #boot.blacklistedKernelModules = [ "i915" ];
 
   networking.hostName = "John-Galois-NixOS"; # Define your hostname.
@@ -27,7 +24,6 @@
     fsType = "exfat";
   };
 
-  services.mbpfan.enable = true;
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "17.03";
