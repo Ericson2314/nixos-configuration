55de9a7ee6814442ace8d4108bbeeaf242ce6b7c
John Ericson <jericson@galois.com>
2017-01-04 11:16:48 -0500

    Finally make some media keys work
    Thanks @vrthra!

--- graphical.nix
+++ graphical.nix
@@ -50,4 +50,13 @@
     windowManager.default = "xmonad";
     desktopManager.default = "none";
   };
+
+  # Media Keys
+  sound.enableMediaKeys = true;
+  services.actkbd.bindings = [
+    { keys = [ 224 ]; events = [ "key" "rep" ]; command = "${pkgs.light}/bin/light -U 4"; }
+    { keys = [ 225 ]; events = [ "key" "rep" ]; command = "${pkgs.light}/bin/light -A 4"; }
+    { keys = [ 229 ]; events = [ "key" "rep" ]; command = "${pkgs.kbdlight}/bin/kbdlight down"; }
+    { keys = [ 230 ]; events = [ "key" "rep" ]; command = "${pkgs.kbdlight}/bin/kbdlight up"; }
+  ];
 }
