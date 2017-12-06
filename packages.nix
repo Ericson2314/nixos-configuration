fe7bc964c4b0a4cdd0d977af5ce880136233a8c5
John Ericson <John.Ericson@Obsidian.Systems>
2017-12-06 17:23:19 -0500

    Make some binary caches use HTTPS

--- packages.nix
+++ packages.nix
@@ -31,12 +31,12 @@
     #package = pkgs.nixUnstable;
     useSandbox = true;
     binaryCaches = [
-      "http://cache.nixos.org/"
+      "https://cache.nixos.org/"
       "https://nixcache.reflex-frp.org"
     ];
     trustedBinaryCaches = [
       "https://hydra.nixos.org/"
-      "http://hydra.cryp.to"
+      "https://hydra.cryp.to"
     ];
     binaryCachePublicKeys = [
       "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
