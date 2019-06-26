a16b58e7a8308f7caff1ce179c24340423a16b25
John Ericson <git@JohnEricson.me>
2019-06-26 09:44:53 -0400

    Manage rust overlay with home-manager
    At this point, we can no need .config in git!

--- user/nixpkgs/default.nix
+++ user/nixpkgs/default.nix
@@ -1,7 +1,20 @@
-{ ... }:
+{ options, ... }:
+
+let
+  nixpkgs-mozilla = ../../dep/nixpkgs-mozilla;
+in
 
 {
+  nixpkgs.overlays = [
+    (import ../../dep/nixpkgs-mozilla/rust-overlay.nix)
+  ];
+  xdg.configFile."nixpkgs/overlays/rust-overlay.nix".text = ''
+    import ${nixpkgs-mozilla}/rust-overlay.nix
+  '';
+
+  xdg.configFile."nixpkgs/dep/nixpkgs-mozilla".source = nixpkgs-mozilla;
+
   nixpkgs.config = import ./config.nix;
   xdg.configFile."nixpkgs/config.nix".source =
-      ./config.nix;
+    ./config.nix;
 }
