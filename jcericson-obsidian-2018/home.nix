5d7112110983fd01ad682466c84c637def179612
John Ericson <John.Ericson@Obsidian.Systems>
2020-02-10 16:44:22 -0500

    Add zoom and telegram to work computer

--- jcericson-obsidian-2018/home.nix
+++ jcericson-obsidian-2018/home.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -10,6 +10,11 @@
   xresources.properties = {
     "*.dpi" = 120;
   };
+
+  home.packages = with pkgs; [
+    zoom-us
+    tdesktop
+  ];
 
   # TODO get alacritty to respect the above, and remove this.
   programs.alacritty.settings.font.size = 4.5;
