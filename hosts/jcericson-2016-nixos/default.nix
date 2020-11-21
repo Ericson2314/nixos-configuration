f0c7373daf932612b6cd0312b5e12ca92b9e9bf7
John Ericson <git@JohnEricson.me>
2020-11-20 21:36:55 -0500

    Finally get some NVIDIA offloading to work!

--- hosts/jcericson-2016-nixos/default.nix
+++ hosts/jcericson-2016-nixos/default.nix
@@ -16,16 +16,34 @@
 
   environment.systemPackages = with pkgs; [
     fbterm # compensate for UHD
+    (pkgs.writeShellScriptBin "nvidia-offload" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec -a "$0" "$@"
+    '')
   ];
-
-  hardware.nvidiaOptimus.disable = true;
 
   nix.maxJobs = 2;
   nix.buildCores = 8;
 
   services.xserver.videoDrivers = [
-      "displaylink"
-    ] ++ options.services.xserver.videoDrivers.default;
+    #"displaylink"
+    #"intel"
+    "nvidia"
+  ];
+  #] ++ options.services.xserver.videoDrivers.default;
+
+  hardware.nvidia.prime = {
+    offload.enable = true;
+
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:1:0:0";
+
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
+  };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
