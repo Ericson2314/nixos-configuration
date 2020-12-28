20846fd5376b84ce49b89e6ff942bb8bbb664fb1
John Ericson <git@JohnEricson.me>
2020-12-28 17:21:38 -0500

    Use nice big console font

--- hosts/jcericson-2016-nixos/hardware-configuration.nix
+++ hosts/jcericson-2016-nixos/hardware-configuration.nix
@@ -44,4 +44,6 @@
 
   nix.maxJobs = lib.mkDefault 8;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  # high-resolution display
+  hardware.video.hidpi.enable = lib.mkDefault true;
 }
