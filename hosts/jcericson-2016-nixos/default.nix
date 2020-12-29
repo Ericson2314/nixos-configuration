347ced550cc19883dc1a99f40c2f37a22b3c738f
John Ericson <git@JohnEricson.me>
2020-12-28 19:56:15 -0500

    Let nixos-hardware handle Nvidia

--- hosts/jcericson-2016-nixos/default.nix
+++ hosts/jcericson-2016-nixos/default.nix
@@ -4,11 +4,10 @@
   imports = [
     ../../system/common.nix
     ../../system/graphical/x.nix
-    ../../system/graphical/nvidia-offload.nix
     ../../system/libinput.nix
     ../../system/video-games.nix
     ../../../hardware-configuration.nix # Include the results of the hardware scan.
-    <nixos-hardware/dell/xps/15-9550> # from the nixos-hardware repo
+    <nixos-hardware/dell/xps/15-9550/nvidia> # from the nixos-hardware repo
   ];
 
   networking = {
@@ -30,14 +29,6 @@
 
   nix.maxJobs = 2;
   nix.buildCores = 8;
-
-  hardware.nvidia.prime = {
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:1:0:0";
-
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:0:2:0";
-  };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
--- hosts/jcericson-obsidian-2018/default.nix
+++ hosts/jcericson-obsidian-2018/default.nix
@@ -4,11 +4,10 @@
   imports = [
     ../../system/common.nix
     ../../system/graphical/x.nix
-    ../../system/graphical/nvidia-offload.nix
     ../../system/libinput.nix
     ../../system/video-games.nix
     ../../../hardware-configuration.nix # Include the results of the hardware scan.
-    <nixos-hardware/dell/xps/15-9550> # from the nixos-hardware repo
+    <nixos-hardware/dell/xps/15-9550/nvidia> # from the nixos-hardware repo
   ];
 
   networking = {
@@ -22,15 +21,6 @@
   environment.systemPackages = with pkgs; [
     fbterm # compensate for UHD
   ];
-
-
-  hardware.nvidia.prime = {
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:1:0:0";
-
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:0:2:0";
-  };
 
   #virtualisation.virtualbox.host.enable = true;
   ##virtualisation.docker.enable = true;
