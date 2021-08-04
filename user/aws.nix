12e4cb8bfbfbd9f85a82f8b7d3682d50c43ea86f
John Ericson <John.Ericson@Obsidian.Systems>
2021-08-04 14:52:39 -0400

    Add little aws ssh proxy helper config

---
+++ user/aws.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  programs.ssh.matchBlocks."i-* mi-*" = {
+    proxyCommand = "aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p'";
+  };
+
+  home.packages = with pkgs; {
+    awscli
+    ssm-session-manager-plugin
+  };
+}
