177de5b0af5009b8213bcd1e7e5dd35a27f6a268
John Ericson <John.Ericson@Obsidian.Systems>
2021-12-13 22:46:10 -0500

    Try bump 21.11
    Nix-thunk is unfortunately broken.

--- user/fish/default.nix
+++ user/fish/default.nix
@@ -2,6 +2,6 @@
 
 {
   programs.fish.enable = true;
-  programs.fish.promptInit = builtins.readFile ./fish_prompt.fish;
+  programs.fish.interactiveShellInit = builtins.readFile ./fish_prompt.fish;
   xdg.configFile."fish/functions".source = ./functions;
 }
