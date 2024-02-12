d932c1b6672b5925fe498d6eb49708e94f4c0da2
John Ericson <John.Ericson@Obsidian.Systems>
2024-02-12 15:51:37 -0500

    Hopefully fix XDG portal settings
    Thanks @cidkidnix.

--- system/graphical/wayland.nix
+++ system/graphical/wayland.nix
@@ -8,10 +8,22 @@
   # Enable screensharing
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
     ];
-    gtkUsePortal = true;
+    config = {
+      common.default = [ "gtk" ];
+      sway.default = [ "gtk" "wlr" ];
+    };
+    wlr = {
+      enable = true;
+      settings = {
+        screencast = {
+          Max_fps = 60;
+          #Output_name = "DP-1";
+          Chooser_type = "default";
+        };
+      };
+    };
   };
 }
