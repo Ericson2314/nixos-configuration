c68730e6370701980ce8dd50945dea3d5baadc85
John Ericson <John.Ericson@Obsidian.Systems>
2024-05-31 18:34:13 -0400

    Remove more `NIX_PATH`

--- hosts/work-ssh-no-ambient-nix/home.nix
+++ hosts/work-ssh-no-ambient-nix/home.nix
@@ -9,10 +9,5 @@
     if [ -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
       . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
     fi
-
-    # Until the above stops setting it, set again
-    export NIX_PATH=${config.home.sessionVariables.NIX_PATH}
   '';
-
-  home.sessionVariables.NIX_PATH = "nixpkgs=${config.home.homeDirectory}/src/nixos-configuration/dep/nixpkgs";
 }
