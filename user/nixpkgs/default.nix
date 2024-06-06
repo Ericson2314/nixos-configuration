1f3584ab31c7474a9d5178143bb108c4756e3862
John Ericson <John.Ericson@Obsidian.Systems>
2024-06-06 12:12:22 -0400

    Make nixpkgs mozilla a thunk not submodule
    ~/.config/nixpkgs is sparser, but that's good.

--- user/nixpkgs/default.nix
+++ user/nixpkgs/default.nix
@@ -1,20 +1,9 @@
 { options, ... }:
 
-let
-  nixpkgs-mozilla = ../../dep/nixpkgs-mozilla;
-in
-
 {
   nixpkgs.overlays = [
-    (import ../../dep/nixpkgs-mozilla/rust-overlay.nix)
+    (import (import ../../dep/nixpkgs-mozilla/thunk.nix + "/rust-overlay.nix"))
   ];
-  xdg.configFile."nixpkgs/overlays/rust-overlay.nix".text = ''
-    import ${nixpkgs-mozilla}/rust-overlay.nix
-  '';
-
-  xdg.configFile."nixpkgs/dep/nixpkgs-mozilla".source = nixpkgs-mozilla;
 
   nixpkgs.config = import ./config.nix;
-  xdg.configFile."nixpkgs/config.nix".source =
-    ./config.nix;
 }
