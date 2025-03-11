ac6bc768999419fa65c59f643b5f70b795fc5d86
John Ericson <John.Ericson@Obsidian.Systems>
2025-03-10 20:24:32 -0400

    Try to LSP in NeoVim

--- user/nvim/default.nix
+++ user/nvim/default.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }:
 
 {
-  programs.neovim.enable = true;
-  programs.neovim.vimAlias = true;
-  programs.neovim.extraConfig = builtins.readFile ./init.vim;
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+    extraConfig = builtins.readFile ./init.vim;
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+      nvim-treesitter.withAllGrammars
+    ];
+  };
+
   # TODO include other neovim settings
   home.file.".vimrc".source = ./init.vim;
 }
