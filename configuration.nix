c1dd7f8424f4fbcccaac0b9a37de41801d0c0828
Samuel Ainsworth <skainsworth@gmail.com>
2025-04-05 19:17:04 -0400

    22.11 stock configuration

--- configuration.nix
+++ configuration.nix
@@ -56,7 +56,7 @@
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.jane = {
+  # users.users.alice = {
   #   isNormalUser = true;
   #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   #   packages = with pkgs; [
@@ -102,6 +102,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
+  system.stateVersion = "22.11"; # Did you read the comment?
 
 }
