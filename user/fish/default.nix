2b92ff993f321e21fd642f9a6ecc5ec7d9de5342
John Ericson <John.Ericson@Obsidian.Systems>
2025-12-30 14:00:30 -0500

    More fish bindings
    Make mod like before, allow ctrl to differ.

--- user/fish/default.nix
+++ user/fish/default.nix
@@ -3,5 +3,21 @@
 {
   programs.fish.enable = true;
   programs.fish.interactiveShellInit = builtins.readFile ./fish_prompt.fish;
-  xdg.configFile."fish/functions".source = ./functions;
+
+  # Swap ctrl and alt left/right arrow keys, backspace, and delete
+  programs.fish.binds = {
+    "ctrl-right".command = "nextd-or-forward-token";
+    "ctrl-left".command = "prevd-or-backward-token";
+    "alt-right".command = "forward-word";
+    "alt-left".command = "backward-word";
+    "ctrl-backspace".command = "backward-kill-token";
+    "alt-backspace".command = "backward-kill-word";
+    "ctrl-delete".command = "kill-token";
+    "alt-delete".command = "kill-word";
+  };
+
+  programs.fish.functions = {
+    notify = builtins.readFile ./functions/notify.fish;
+    git-filter-sed = builtins.readFile ./functions/git-filter-sed.fish;
+  };
 }
