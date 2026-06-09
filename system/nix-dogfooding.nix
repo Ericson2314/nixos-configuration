594c0c77c05fb6703635feb8c609d01b5bb67cae
John Ericson <John.Ericson@Obsidian.Systems>
2026-06-09 13:24:39 -0400

    Make it possible to run NixOS container tests
    Need to turn on some experimental features.

--- system/nix-dogfooding.nix
+++ system/nix-dogfooding.nix
@@ -23,4 +23,9 @@ in
 {
   # Dogfooding master! 🫡
   nix.package = nixComponents.nix-cli;
+
+  # Want to use the new containers tests
+  nix.settings.extra-experimental-features = [ "auto-allocate-uids" "cgroups" ];
+  nix.settings.auto-allocate-uids = true;
+  nix.settings.use-cgroups = true;
 }
