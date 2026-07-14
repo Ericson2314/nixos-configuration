14b23a1d95c490765db60562fc291f6af30b1905
John Ericson <John.Ericson@Obsidian.Systems>
2026-07-14 10:54:29 -0400

    26.06 stock configuration

--- configuration.nix
+++ configuration.nix
@@ -115,6 +115,6 @@
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "26.05"; # Did you read the comment?
 
 }
